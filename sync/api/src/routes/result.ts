3707953d733e4d55e30045bfa0c435d8e360872d
vamos-23 <172397434+vamos-23@users.noreply.github.com>
2026-03-13 00:12:10 +0530

    feat: added try-catch logic in results route

--- sync/api/src/routes/result.ts
+++ sync/api/src/routes/result.ts
@@ -2,11 +2,17 @@ import { Router } from "express";
 import prisma from "../db";
 
 const router = Router();
+
 router.get("/:id", async (req, res) => {
-  const { id: jobId } = req.params;
-  const job = await prisma.job.findUnique({ where: { jobId } });
-  if (!job) {
-    return res.status(400).json({ error: "No job found" });
+  try {
+    const { id: jobId } = req.params;
+    const job = await prisma.job.findUnique({ where: { jobId } });
+    if (!job) {
+      return res.status(400).json({ error: "No job found" });
+    }
+    return res.json(job);
+  } catch (error) {
+    console.log("Result fetch error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-  return res.json(job);
 });
